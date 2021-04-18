30c494f300cbf1cc2dcfbc4933163d1877d7e90d
zgsuppo <dsuppo@zerogrey.com>
2021-04-18 23:09:29 +0200

    Bump to 0.0.4 with new translations and first converters

--- it-IT/babele-register.js
+++ it-IT/babele-register.js
@@ -10,8 +10,7 @@ function parseSkill(value){
 	else return value;
 }
 
-function parseHindrance(value){
-	return "Hello world!";
+function parseName(value){
 	//Parse the translated compendiums to find a match
 	let item = {};
 	item['_id'] = value;
@@ -24,10 +23,22 @@ function parseHindrance(value){
 }
 
 
+function parseRequirements(value){
+	//Parse the translated compendiums to find a match
+	let item = {};
+	item['_id'] = value;
+	let pack = game.packs.find(pack => pack.translated && pack.hasTranslation(item));
+	if(pack) {
+		return pack.translate(item, true).requirements;
+	}
+	//Otherwise return current value
+	return value;
+}
+
 Hooks.once('init', () => {
 
 	if(typeof Babele !== 'undefined') {
-		
+
 		Babele.get().register({
 			module: 'swade-it',
 			lang: 'it',
@@ -36,7 +47,8 @@ Hooks.once('init', () => {
 
 		Babele.get().registerConverters({
 			"translateSkill": (value) => parseSkill(value),
-			"translateHindrance": (value) => parseHindrance(value)
+			"translateName": (value) => parseName(value),
+			"translateRequirements": (value) => parseRequirements(value),
 		});
 	}
 });
